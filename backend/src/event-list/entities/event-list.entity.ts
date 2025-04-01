5ec8da550665c4b0eb0c4b59179ccc30fe55f20d
Sebastian-Klingbeil <klingbeil-sebastian@hotmail.de>
2025-04-01 06:32:51 +0200

    add event-list entity

--- backend/src/event-list/entities/event-list.entity.ts
+++ backend/src/event-list/entities/event-list.entity.ts
@@ -1,1 +1,11 @@
-export class EventList {}
+/* eslint-disable prettier/prettier */
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class EventList {
+    @PrimaryGeneratedColumn()
+    eventId: number;
+
+    @Column()
+    userName: number;
+}
--- backend/src/event-list/event-list.controller.ts
+++ backend/src/event-list/event-list.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { EventListService } from './event-list.service';
 import { CreateEventListDto } from './dto/create-event-list.dto';
